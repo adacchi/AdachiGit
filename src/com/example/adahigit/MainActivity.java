b315927e0283818617ec568dd27286a1a7368d1b
安達 誠寛 <adachinobuhiro@adachi-no-MacBook-Air.local>
2013-12-19 20:12:28 +0900

    コメントを追加

--- src/com/example/adahigit/MainActivity.java
+++ src/com/example/adahigit/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.adahigit;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-
+/**
+ * ‚±‚ê‚ÍGit‚Ì—ûK‚Ì‚½‚ß‚ÌƒRƒƒ“ƒg‚Å‚·B
+ * @author adachinobuhiro
+ *
+ */
 public class MainActivity extends Activity {
 
 	@Override
